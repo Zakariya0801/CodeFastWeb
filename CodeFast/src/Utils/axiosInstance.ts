eb488c8fc3707904c91aa3d8ada4a882d8804b56
Zakariya Abbas <i220801@nu.edu.pk>
2025-04-18 21:12:46 +0500

    Deployment links

--- CodeFast/src/Utils/axiosInstance.ts
+++ CodeFast/src/Utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api", // Update with your backend's base URL
+  baseURL: "https://codefast-web.onrender.com/api", // Update with your backend's base URL
   headers: {
     "Content-Type": "application/json",
   },
